8610fb297ce855447d25b6ae0742614927efe86a
Zahra Alzawad <zahra_m.alzawad@hotmail.com>
2021-12-05 10:19:23 +0000

    ..

--- cold call/ViewController.swift
+++ cold call/ViewController.swift
@@ -9,16 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     let namesArray = ["Zahra","Noor","Sara","Huda","Rawan"]
     override func viewDidLoad() {
         super.viewDidLoad()
         nameLabel.text = "Ready?"
+        numLabel.isHidden = true
     }
 
     @IBAction func getNamesRandomlly(_ sender: Any) {
         let index = Int.random(in: 0..<5)
         nameLabel.text = namesArray[index]
+        let num = Int.random(in: 1...5)
+        switch num {
+        case 1...2 :
+            numLabel.textColor = UIColor.red
+        case 3...4:
+            numLabel.textColor = UIColor.orange
+        default:
+            numLabel.textColor = UIColor.green
+        }
+        numLabel.isHidden = false
+        numLabel.text = String(num)
     }
     
 }
